696f02079ed0863a864e253a2a1baf43ffb68f4b
Benjamin Namikas <ben.namikas@gmail.com>
2025-09-21 13:11:19 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { View, ActivityIndicator, Text } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { Colors } from './src/constants/Colors';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
